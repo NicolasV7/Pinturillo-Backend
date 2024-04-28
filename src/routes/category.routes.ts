2941e27e8e0a2476ad729a75a3c0e403a2aa6c24
Nicolas <nicolas@dlpbasculas.com>
2024-04-27 19:29:53 -0500

    Add category routes

---
+++ src/routes/category.routes.ts
@@ -0,0 +1,13 @@
+import * as express from 'express';
+import { CategoryController } from '../controllers/category.controller';
+
+const router = express.Router();
+const categoryController = new CategoryController();
+
+router.get('/category/:name', categoryController.getByName);
+router.get('/category/:id', categoryController.getById);
+router.get('/category', categoryController.getAll);
+router.post('/category', categoryController.create);
+router.delete('/category', categoryController.delete);
+
+export { router as categoryRoutes}
