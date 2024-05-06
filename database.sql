c5637e441773226f7ef0912187d30c20a7119ddb
NicolasV7 <nicolas@dlpbasculas.com>
2024-05-06 15:21:52 -0500

    Refactor database schema and add foreign key constraints

--- database.sql
+++ database.sql
@@ -15,18 +15,22 @@ CREATE TABLE CATEGORY (
 	name VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE WORD_CATEGORY (
-	id uuid DEFAULT uuid_generate_v4 () PRIMARY KEY,
-	id_category int,
-	id_word int,
-	FOREIGN KEY (id_category) REFERENCES CATEGORY(id),
-	FOREIGN KEY (id_word) REFERENCES WORD(id)
-);
-
 CREATE TABLE GAME_ROOM (
 	id uuid DEFAULT uuid_generate_v4 () PRIMARY KEY,
 	room_name VARCHAR(100) NOT NULL,
 	state VARCHAR(100) NOT NULL,
-	id_category int,
-	FOREIGN KEY (id_category) REFERENCES CATEGORY(id)
+	id_category uuid
 );
+
+CREATE TABLE WORD_CATEGORY (
+	id uuid DEFAULT uuid_generate_v4 () PRIMARY KEY,
+	id_category uuid,
+	id_word uuid,
+);
+
+ALTER TABLE WORD_CATEGORY
+	ADD FOREIGN KEY (id_category) REFERENCES CATEGORY(id),
+	ADD FOREIGN KEY (id_word) REFERENCES WORD(id);
+
+ALTER TABLE GAME_ROOM
+	ADD FOREIGN KEY (id_category) REFERENCES CATEGORY(id);
