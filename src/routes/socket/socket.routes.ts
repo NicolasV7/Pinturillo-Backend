510f787b5801a9624cac074fcfddb68ba63528f0
NicolasV7 <nicolas@dlpbasculas.com>
2024-05-07 12:16:36 -0500

    Refactor code in socket.routes.ts to improve readability and maintainability

--- src/routes/socket/socket.routes.ts
+++ src/routes/socket/socket.routes.ts
@@ -26,82 +26,98 @@ module.exports = (expressWs) => {
 
     let usersPlay = socketController.assingTurn(idRoom);
 
-    const userPlayTurn = await (socketController.playerTurn(idRoom, ws));
+    const userPlayTurn = await socketController.playerTurn(idRoom, ws);
 
-    SocketController.rooms[idRoom].forEach(user => {
-      if(user.ws === ws) {
+    SocketController.rooms[idRoom].forEach((user) => {
+      if (user.ws === ws) {
         user.ws.send(`Your turn to play ${userPlayTurn}`);
       }
-      if(user.ws !== ws && user.ws.readyState === ws.OPEN) {
+      if (user.ws !== ws && user.ws.readyState === ws.OPEN) {
         user.ws.send(`[+] ${userName} has joined the room`);
       }
     });
-    if(SocketController.rooms[idRoom].size === 1) {
-      SocketController.rooms[idRoom].forEach(user => {
+    if (SocketController.rooms[idRoom].size === 1) {
+      SocketController.rooms[idRoom].forEach((user) => {
         user.ws.send(`[+] Waiting for more players to join the room`);
       });
     }
+
     ws.on("message", async (msg) => {
       const jsonMessage = JSON.parse(msg);
-      if(jsonMessage.type === "SEND_MESSAGE") {
+      if (jsonMessage.type === "SEND_MESSAGE") {
         SocketController.rooms[idRoom].forEach(async user => {
-          const wordMessage = socketController.guessWord(idRoom, jsonMessage.data);
+          const wordMessage = socketController.guessWord(
+            idRoom,
+            jsonMessage.data
+          );
 
-          if(user.ws == ws && user.ws.readyState === ws.OPEN && wordMessage) {
+          if (user.ws == ws && user.ws.readyState === ws.OPEN && wordMessage) {
             user.ws.send(`[+] You have guessed the word`);
             solveWord.push(user.ws);
-            const score = await socketController.score(idRoom, solveWord.length-1, ws, time);
+            const score = await socketController.score(
+              idRoom,
+              solveWord.length - 1,
+              ws,
+              time
+            );
             user.ws.send(`[+] Your score is: ${score}`);
           }
 
-          if(user.ws !== ws && user.ws.readyState === ws.OPEN) {
-            if(wordMessage) {
+          if (user.ws !== ws && user.ws.readyState === ws.OPEN) {
+            if (wordMessage) {
               user.ws.send(`[+] ${userName} has guessed the word`);
             } else {
               user.ws.send(`[+] ${userName}: ${jsonMessage.data}`);
             }
           }
 
-          if(solveWord.length === SocketController.rooms[idRoom].size) {
-            SocketController.rooms[idRoom].forEach(user => {
-              if(user.ws.readyState === ws.OPEN) {
+          if (solveWord.length === SocketController.rooms[idRoom].size) {
+            SocketController.rooms[idRoom].forEach((user) => {
+              if (user.ws.readyState === ws.OPEN) {
                 user.ws.send(`[+] Round finished`);
               }
             });
           }
         });
-      }
-      else if(jsonMessage.type === "START_GAME" && SocketController.rooms[idRoom].size > 1 && room.state === 'En curso') {
+      } else if (
+        jsonMessage.type === "START_GAME" &&
+        SocketController.rooms[idRoom].size > 1 &&
+        room.state === "En curso"
+      ) {
         await startGame();
       }
     });
     async function startGame() {
-      try{
+      try {
         const asignWord = await socketController.asignWordToGuess(idRoom);
         const users = Array.from(SocketController.rooms[idRoom]);
         let user = "";
 
         const userPromise = users.map(async (user: any) => {
           const turn = await socketController.playerTurn(idRoom, user.ws);
-          if(user.ws.readyState === ws.OPEN && turn === 1) {
+          if (user.ws.readyState === ws.OPEN && turn === 1) {
             user = user.userName;
             solveWord.push(user.ws);
             user.ws.send(`[+] Your turn to play`);
             user.ws.send(`[+] Word to draw: ${asignWord}`);
 
             usersPlay = socketController.endTurn(idRoom);
-          }else{
+          } else {
             user.ws.send(`[+] ${userName} is drawing`);
           }
+
           const limitTime = 90;
           let timeController = limitTime;
           return new Promise<void>((resolve) => {
             const interval = setInterval(() => {
               timeController--;
               time = timeController;
-              if(timeController > 0 && solveWord.length < SocketController.rooms[idRoom].size) {
+              if (
+                timeController > 0 &&
+                solveWord.length < SocketController.rooms[idRoom].size
+              ) {
                 user.ws.send(`[+] Time left: ${timeController}`);
-              }else{
+              } else {
                 clearInterval(interval);
                 user.ws.send(`[+] Time is over`);
                 resolve();
@@ -111,19 +127,19 @@ module.exports = (expressWs) => {
         });
         await Promise.all(userPromise);
         finishTurn();
-      }catch(error){
+      } catch (error) {
         console.log(error);
       }
     }
 
     async function finishTurn() {
       roomRounds--;
-      if(roomRounds > 0) {
+      if (roomRounds > 0) {
         solveWord = [];
         return await startGame();
-      }else{
-        SocketController.rooms[idRoom].forEach(user => {
-          if(user.ws.readyState === ws.OPEN) {
+      } else {
+        SocketController.rooms[idRoom].forEach((user) => {
+          if (user.ws.readyState === ws.OPEN) {
             socketController.endGame(idRoom, user.ws);
             user.ws.send(`[+] Game finished`);
           }
@@ -133,25 +149,27 @@ module.exports = (expressWs) => {
     }
 
     ws.on("close", async () => {
-      try{
+      try {
         SocketController.rooms[idRoom].delete(ws);
-        if(SocketController.rooms[idRoom].size === 0) {
+        if (SocketController.rooms[idRoom].size === 0) {
           delete SocketController.rooms[idRoom];
         }
         socketController.leaveRoom(ws, idRoom);
         usersPlay = socketController.assingTurn(idRoom);
 
-        SocketController.rooms[idRoom].forEach(user => {
-          if(user.ws.readyState === ws.OPEN) {
-            user.ws.send(`[+] ${userName} has left the room`);
-            const userPlayTurn = socketController.playerTurn(idRoom, user.ws);
-            user.ws.send(`[+] Next player to play: ${userPlayTurn}`);
+        SocketController.rooms[idRoom].forEach((user) => {
+          if (user.ws.readyState === ws.OPEN) {
+            user.ws.send(`[-] ${userName} has left the room`);
+            if (SocketController.rooms[idRoom].size > 1) {
+              const userPlayTurn = socketController.playerTurn(idRoom, user.ws);
+              user.ws.send(`[+] Next player to play: ${userPlayTurn}`);
+            }
           }
         });
-      } catch(error) {
+      } catch (error) {
         console.log(error);
       }
     });
   });
   return router;
-}
+};
